73a55542bd6ef5d4f9b65172ca4f44e59c51c3c1
Ra100 <ra100x@gmail.com>
2015-10-19 13:40:37 +0200

    comments

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 var cheerio = require("cheerio"),
   http = require("http"),
   PushBullet = require('pushbullet'),
-  config = require('./config.json'),
-  pusher = new PushBullet(config.pushbulletApiKey);
+  config = require('./config.json');
 
+// pushbullet object
+var pusher = new PushBullet(config.pushbulletApiKey);
+// refresh interval
 var interval;
 
 // Utility function that downloads a URL and invokes
 // callback with the data.
+/**
+ * Utility function that downloads a URL and invokes callback with the data.
+ * @param  {string}   url      URL location
+ * @param  {Function} callback executes after contents are loaded
+ * @return {null}
+ */
 function download(url, callback) {
   http.get(url, function (res) {
     var data = "";
@@ -22,6 +30,10 @@ function download(url, callback) {
   });
 }
 
+/**
+ * Download url and check content.
+ * @return null
+ */
 function checkSchedule() {
   download(config.url, function (data) {
     if (data) {
@@ -33,21 +45,36 @@ function checkSchedule() {
       }
     }
   });
+  return;
 }
 
+/**
+ * Pushes link with info, that new schedule is available.
+ * @return null
+ */
 function sendNotification() {
   pusher.link('', "New schedule", config.cinemaLink, function (error, response) {
     console.log('New program. Push sent.');
     process.exit(0);
   });
+  return;
 }
 
+/**
+ * Push note message showing that script still runs.
+ * @return null
+ */
 function sendPing() {
   pusher.note('', "Cinema loder", "Just sain' I'm still running.", function (error, response) {
     console.log('Still running.');
   });
+  return;
 }
 
+/**
+ * App init function.
+ * @return null
+ */
 function run() {
   console.log('Checking started.');
   interval = setInterval(checkSchedule, 60 * 1000);
@@ -55,6 +82,8 @@ function run() {
     var pingInterval = setInterval(sendPing, config.pingInterval * 1000);
   }
   checkSchedule();
+  return;
 }
 
+// start script
 run();
